5fbe0e3f3ddebcf9b570aa01d4f8e35958d2092d
riyagogia50 <riyagogia50@gmail.com>
2024-09-13 11:11:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1350,7 +1350,7 @@ function detectionRealisationDeOuf(dir, action) {
     $("body").removeClass("item-cat").addClass("brilliant");
      $("#ic").remove();
      window.setTimeout(function(){
-    $("#btn").removeClass("#ic").append("<a id='ic' href='https://github.com/ayseth/brilliant' target='_blank'>Check source code</a><a id='ic2' href='sites/101brilliant/brilliant.html' target='_blank'>View page</a>");
+    $("#btn").removeClass("#ic").append("<a id='ic' href='https://github.com/ayseth/brilliant' target='_blank'>Click here</a><a id='ic2' href='sites/101brilliant/brilliant.html' target='_blank'>View page</a>");
     // $("#btn").remove("button").append("{delay=false;}");
   {delay=false;}
   } ,800);
@@ -1370,7 +1370,7 @@ function detectionRealisationDeOuf(dir, action) {
      window.setTimeout(function(){
       // $("#ic").remove();
       $("#ic2").remove();
-     $("#btn").removeClass("#ic #ic2").append("<a id='ic' href='https://github.com/ayseth/cms-blog' target='_blank'>Check source code</a>");
+     $("#btn").removeClass("#ic #ic2").append("<a id='ic' href='https://vsco.co/riyagogia-12/gallery' target='_blank'>Click here</a>");
     {delay=false;}
     } ,800);
 
@@ -1417,7 +1417,7 @@ $("#ic2").remove();
 
     window.setTimeout(function(){
      
-    $("#btn").removeClass("#ic #ic2").append("<a id='ic' href='https://github.com/ayseth/brilliant' target='_blank'>Check source code</a><a id='ic2' href='sites/101brilliant/brilliant.html' target='_blank'>View page</a>");
+    $("#btn").removeClass("#ic #ic2").append("<a id='ic' href='https://vsco.co/riyagogia-12/gallery' target='_blank'>Click here</a><a id='ic2' href='sites/101brilliant/brilliant.html' target='_blank'>View page</a>");
         // $("#btn").remove("button").append("");
      {delay=false;}
       } ,800);
